85abd75caff211dc3c942c04bc8e83edc05524ee
Xstunter <bdatsenko2@ukr.net>
2023-10-13 21:21:09 +0300

    HomeWork2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using System;
+
+int[] mas = new int[100];
+Random rand = new Random();
+int count = 0;
+
+Console.WriteLine("Array range");
+
+Console.Write("Lower number of the array:");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Higher number of the array:");
+int max = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = rand.Next(min, max);
+    if (mas[i] >= -100 && mas[i] <= 100)
+    {
+        count++;
+        Console.WriteLine($"Number in the range -100 to +100: {mas[i]}");
+    }
+}
+Console.WriteLine($"Determine the number of elements whose values are in the range - 100 to + 100 is: {count}");
+
+Console.ReadLine();
+
+
---
+++ Lesson2_ex2/Lesson2_ex2/Program.cs
@@ -0,0 +1,61 @@
+﻿int[] masA = new int[20];
+Random rand = new Random();
+
+Console.WriteLine("Array range");
+
+Console.Write("Lower number of the array:");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Higher number of the array:");
+int max = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < masA.Length; i++)
+{
+    masA[i] = rand.Next(min, max);
+    if (masA[i] <= 888)
+    {
+        count++;
+    }
+}
+
+
+int[] masB = new int[count];
+int n = 0;
+
+for (int i = 0; i < masA.Length; i++)
+{
+    if (masA[i] <= 888)
+    {
+        masB[n] = masA[i];
+        n++;
+    }
+}
+
+int sort;
+
+for (int i = 0; i < masB.Length; i++)
+{
+    for (int j = i + 1; j < masB.Length; j++)
+    {
+        if (masB[i] < masB[j])
+        {
+            sort = masB[j];
+            masB[j] = masB[i];
+            masB[i] = sort;
+        }
+    }
+}
+
+/*Array.Sort(masB);
+Array.Reverse(masB);*/
+
+Console.WriteLine($"Array masB[{count}] in descending order:");
+
+for (int i = 0; i < masB.Length; i++)
+{
+    Console.Write(masB[i] + " ");
+}
+
+Console.ReadLine();
