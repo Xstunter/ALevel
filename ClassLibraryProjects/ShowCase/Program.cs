0e977096f2fdc64992b94e81a3ebdca553c1436f
Xstunter <bdatsenko2@ukr.net>
2023-10-13 21:55:10 +0300

    Added hw1

---
+++ ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,44 @@
+﻿using UtilityLibraries;
+using System;
+
+int row = 0;
+
+do
+{
+    if (row == 0 || row >= 25)
+    {
+        ResetConsole();
+    }
+
+    var input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input))
+    {
+        break;
+    }
+
+    Console.WriteLine($"Input: {input}");
+    Console.WriteLine("Begins with uppercase? " +
+                      $"{(input.StartWithUpper() ? "Yes" : "No")}");
+    Console.WriteLine();
+
+    row += 4;
+
+} while (true);
+return;
+void ResetConsole()
+{
+    if(row > 0)
+    {
+        Console.WriteLine("Press any key to countinue...");
+        Console.ReadKey();
+    }
+
+    Console.Clear();
+    Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit;" +
+                      $" otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+}
+
+
+
+
+
