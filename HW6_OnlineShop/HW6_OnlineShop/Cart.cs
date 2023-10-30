46c09b5af1a88bc5a94efaddbc702cea46072f10
Bogdan <bdatsenko2@ukr.net>
2023-10-30 12:47:32 +0200

    HW6 OnlineShop

---
+++ HW6_OnlineShop/HW6_OnlineShop/Cart.cs
@@ -0,0 +1,63 @@
+﻿namespace HW6_OnlineShop
+{
+    internal class Cart
+    {
+        public List<string> clientProducts = new List<string>();
+
+        public List<double> priceProduct = new List<double>();
+        public void GetProduct()
+        {
+            Products dish = new Products();
+
+            bool confirm = false;
+
+            do 
+            {
+                dish.ShowProduct(Products.products);
+
+                Console.WriteLine();
+
+                dish.ShowPrice(dish.price);
+
+                Console.WriteLine();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Enter product what do you want in 1 to {Products.products.Length}:");
+
+                    int product = int.Parse(Console.ReadLine());
+
+                    clientProducts.Add(dish[product - 1]);
+
+                    priceProduct.Add(dish.price[product - 1]);
+                }
+
+                dish.ShowProduct(clientProducts);
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Total price: {priceProduct.Sum()}грн.");
+
+                Console.WriteLine("Confirm your order. Y or N.");
+
+                if (Console.ReadLine() == "Y")
+                {
+                    Random random = new Random();
+                    int orderID = random.Next(int.MinValue + int.MaxValue, int.MaxValue);
+                    confirm = true;
+                    Console.WriteLine($"Your order confirm! OrderID:{orderID}");
+                }
+                else
+                { 
+                    clientProducts.Clear();
+                    priceProduct.Clear();
+                    Console.Clear();
+                }
+
+               
+            }
+            while (!confirm);
+        }
+
+    }
+}
---
+++ HW6_OnlineShop/HW6_OnlineShop/Products.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+namespace HW6_OnlineShop
+{
+    internal class Products
+    {
+        public static string[] products =
+        {
+            "Lazanya",
+            "Borch",
+            "Solyanka",
+            "Pure",
+            "Harcho",
+            "Napoleon",
+            "CocaCola",
+            "Moxito"
+        };
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= products.Length - 1)
+                {
+                    return products[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Not correct index!");
+                }
+            }
+        }
+
+        public double[] price =
+        {
+            42.55,
+            30,
+            40.25,
+            20,
+            50,
+            34,
+            20,
+            30
+        };
+        public void ShowProduct(List<string> product) 
+        {
+            Console.WriteLine("Choosed products:");
+
+            foreach (var item in product)
+            {
+                Console.Write(item + " ");
+            }        
+        }
+
+        public void ShowProduct(string[] product)
+        {
+            foreach (var item in product)
+            {
+                Console.Write($"{item,-8}\t");
+            }
+        }
+
+        public void ShowPrice(double[] mas)
+        {
+            foreach (var item in mas)
+            {
+                Console.Write($"{item}грн.   \t");
+            }
+        }
+    }
+}
