5ef06b7ac2a55a3926a7e0158b33914023890413
Bogdan <bdatsenko2@ukr.net>
2023-11-25 21:19:35 +0200

    HW3.3 Delegate

---
+++ HW_3.3_Delegate/HW_3.3_Delegate/Classes/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW_3._3_Delegate.Classes
+{
+    public delegate bool DelegateResult(double number);
+    public class Class2
+    {
+        private double result;
+        public DelegateResult Calc(DelegateMultiply valueDelegate, double x, double y)
+        {
+            result = valueDelegate(x, y);
+            return Result;
+        }
+        public bool Result(double number)
+        {
+            return result % number == 0;
+        }
+    }
+}
