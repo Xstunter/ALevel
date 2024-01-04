f4a722e5106984ecb1e88c8961c687cb34f03449
Bogdan <bdatsenko2@ukr.net>
2024-01-04 18:34:59 +0200

    HW4.4 EntityFreamwork

---
+++ HW4.4_EntityFreamwork/HW4.4_EntityFreamwork/AnimalsApplicationsContextFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace HW4._4_EntityFreamwork
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    public sealed class AnimalsApplicationsContextFactory : IDesignTimeDbContextFactory<AnimalsApplicationContext>
+    {
+        public AnimalsApplicationContext CreateDbContext(string[] args)
+        {
+            var connectionString =
+    "Data Source=DESKTOP-1MPCTDH\\SQLEXPRESS;Initial Catalog=Animals;Integrated Security=True; TrustServerCertificate=True";
+            var optionBuilder = new DbContextOptionsBuilder<AnimalsApplicationContext>();
+            var options = optionBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+            return new AnimalsApplicationContext(options);
+        }
+    }
+}
---
+++ HW4.4_EntityFreamwork/HW4.4_EntityFreamwork/DbModels/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HW4._4_EntityFreamwork.DbModels
+{
+    public sealed class Category
+    {
+        public string Id { get; set; }
+        public string CategoryName { get; set; }
+        public List<Pet> Pets { get; set; } //navigetion property
+        public List<Breed> Breeds  { get; set; } //navigetion property
+    }
+}
