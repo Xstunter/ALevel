f88a1898f387d3caaafad034d93484246d0e7bf5
Bogdan <bdatsenko2@ukr.net>
2024-02-21 15:40:51 +0200

    HW6.1

---
+++ Model_6/HW6.1_MicroService/eShop-Sample2/Catalog/Catalog.Host/Controllers/GameForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameForecastController : ControllerBase
+    {
+        private static readonly string[] Games = new[]
+        {
+        "PathOfExile", "CsGo", "CallDuty", "Chess", "BattleField", "Dota",
+        "Diablo", "MarvelSnap", "CrusaderKings"
+        };
+
+        [HttpGet (Name = "Test")]
+        public string[] Get()
+        {
+            return Games;
+        }
+    }
+}
