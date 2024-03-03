5982685e0f1a948742448a2c19fd00bb7c1d50c2
Bogdan <bdatsenko2@ukr.net>
2024-03-03 14:25:05 +0200

    HW6.3 UnitTests

---
+++ Model_6/HW6.3_UnitTest/eShop-Sample3/Catalog/Catalog.Host/Repositories/Interfaces/ICatalogTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Host.Data.Entities;
+using Catalog.Host.Data;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICatalogTypeRepository
+    {
+        Task<PaginatedItems<CatalogType>> GetByPageAsync(int pageIndex, int pageSize);
+        Task<int?> Add(string type);
+        Task Update(int id, string type);
+        Task Delete(int id);
+    }
+}
---
+++ Model_6/HW6.3_UnitTest/eShop-Sample3/Catalog/Catalog.Host/Services/Interfaces/ICatalogBrandService.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Host.Services.Interfaces
+{
+    public interface ICatalogBrandService
+    {
+        Task<int?> Add(string brand);
+        Task Update(int id, string brand);
+        Task Delete(int id);
+    }
+}
